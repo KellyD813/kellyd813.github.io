087d2ae2ff0842c0f8c07477400f9c7fed269d78
Kelly Devine <kellyd813@gmail.com>
2020-05-31 21:52:03 -0700

    adding in a new project

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 window.projects = {
-  'L\Oreal Paris - Elvive Hair Routine Finder' : 'https://www.kellyd813.com/projects/loreal-paris-elvive-hair-routine-finder/main.html'
+    'L\Oreal Paris - Elvive Hair Routine Finder': 'https://www.kellyd813.com/projects/loreal-paris-elvive-hair-routine-finder/main.html'
+    'Resolution Project - 2017': 'https://www.resolutionproject2017.com/'
 }
